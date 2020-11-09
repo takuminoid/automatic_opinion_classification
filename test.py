68ce0a50e407ed510114b9decbe003b8983ba299
Takumi Sato <takutaku9090@gmail.com>
2020-11-09 11:51:48 +0900

    add テスト追記
    数字が全角から半角

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 from classificate_opinions import ClassificateOpinions
 
 c = ClassificateOpinions(['こんにちは。・おはよう', 'こんばんは'])
-out = c.text_splitter(['こんにちは。・おはよう', '①こんばんは②そうなのですか'])
-if out == ['こんにちは', 'おはよう', 'こんばんは', 'そうなのですか']:
-  print('ok')
+out = c.text_splitter(['こんにちは。・１２おはよう１２', '①こんばんは②そうなのですか'])
+ans = ['こんにちは', '12おはよう12', 'こんばんは', 'そうなのですか']
+if out == ans:
+  print('ok1')
 else:
   print('error1')
-  print('out is', out)
+  print('expected is ',ans, ', your out is', out)
