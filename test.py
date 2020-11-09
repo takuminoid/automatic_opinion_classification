275ba5047aa970b1e4c0d8e69d788b740d52d632
Takumi Sato <takutaku9090@gmail.com>
2020-11-09 12:07:46 +0900

    add テストの追加

--- test.py
+++ test.py
@@ -1,10 +1,21 @@
 from classificate_opinions import ClassificateOpinions
 
 c = ClassificateOpinions(['こんにちは。・おはよう', 'こんばんは'])
-out = c.text_splitter(['こんにちは。・１２おはよう１２', '①こんばんは②そうなのですか'])
-ans = ['こんにちは', '12おはよう12', 'こんばんは', 'そうなのですか']
+
+# def text_spiliter
+out = c.text_splitter(['こんにちは。・おはよう', '①こんばんは②そうなのですか'])
+ans = ['こんにちは', 'おはよう', 'こんばんは', 'そうなのですか']
 if out == ans:
   print('ok1')
 else:
   print('error1')
+  print('expected is ',ans, ', your out is', out)
+
+# def num_zen_to_han
+out = c.num_zen_to_han(['12おはよう', '３４です', 'おは５6'])
+ans = ['12おはよう', '34です', 'おは56']
+if out == ans:
+  print('ok2')
+else:
+  print('error2')
   print('expected is ',ans, ', your out is', out)
