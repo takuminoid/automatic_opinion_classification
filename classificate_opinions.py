6f06ae657bfc10fc46e3159f57b7a5e015d6383d
Takumi Sato <takutaku9090@gmail.com>
2020-11-09 12:36:48 +0900

    clean
    run autopep8

--- classificate_opinions.py
+++ classificate_opinions.py
@@ -1,65 +1,67 @@
 import mojimoji as mj
 import copy
 
+
 class ClassificateOpinions():
-  '''
-    概要：意見を入力すると，自動的に分類ラベルの予測，意見分類を行う
-    Input: 意見のリスト　例）['こんにちは', '今日はいい天気ですね', '私は原発には反対です']
-    Output: どのラベルに分類されたかのリスト，全ラベルのリスト　例）{
-                                                            label_num: [1, 2, 0],
-                                                            labels: ['[原発]', '[賛成]', '[天気]'],
-                                                            }
-  '''
-  def __init__(self, opinions):
-    self.opinions = opinions
+    '''
+      概要：意見を入力すると，自動的に分類ラベルの予測，意見分類を行う
+      Input: 意見のリスト　例）['こんにちは', '今日はいい天気ですね', '私は原発には反対です']
+      Output: どのラベルに分類されたかのリスト，全ラベルのリスト　例）{
+                                                              label_num: [1, 2, 0],
+                                                              labels: ['[原発]', '[賛成]', '[天気]'],
+                                                              }
+    '''
 
-  def classificate(self):
-    pass
+    def __init__(self, opinions):
+        self.opinions = opinions
 
-  def text_cleaning(self, opinions):
-    splitted_opinions = self.text_splitter(self.opinions) # 意見の分割
-    splitted_opinions = self.num_zen_to_han(splitted_opinions) # 数字の全角を半角へ
-    # ストップワーズの除去
-    ngwords = []
-    stopwords = open('stopwords.txt', 'r') # SlothLib + etc
-    for line in stopwords:
-      ngwords.append(line.rstrip('\n'))
-    stopwords.close()
-    for i in range(12353, 12436):#平仮名一文字
-        ngwords.append(chr(i))
-    for i in range(12449, 12533):#片仮名一文字
-        ngwords.append(chr(i))
-    for i in range(97, 97+26):#アルファベット小文字
-        ngwords.append(chr(i))
-    for i in range(65, 65+26):#アルファベット大文字
-        ngwords.append(chr(i))
-    ngwords_origin = copy.deepcopy(ngwords)
-    pass
+    def classificate(self):
+        pass
 
-  def text_splitter(self, opinions):
-    splitted_opinions = []
-    split_list = ['。・', '？・']
-    split_list2 = ['②', '③', '④', '⑤', '⑥', '⑦', '⑧', '⑨']
-    for opinion in opinions:
-      flag = False
-      if '①' in opinion:
-        for spliter in split_list2:
-          if spliter in opinion:
-            opinion_buf = opinion.split(spliter)
-            splitted_opinions.append(opinion_buf[0][1:])
-            opinion = opinion_buf[1]
-            flag = True
-        splitted_opinions.append(opinion)
-      if not flag:
-        for spliter in split_list:
-          if spliter in opinion:
-            splitted_opinions.extend(opinion.split(spliter))
-            flag = True
-      if not flag:
-        splitted_opinions.append(opinion)
-    return splitted_opinions
+    def text_cleaning(self, opinions):
+        splitted_opinions = self.text_splitter(self.opinions)  # 意見の分割
+        splitted_opinions = self.num_zen_to_han(splitted_opinions)  # 数字の全角を半角へ
+        # ストップワーズの除去
+        ngwords = []
+        stopwords = open('stopwords.txt', 'r')  # SlothLib + etc
+        for line in stopwords:
+            ngwords.append(line.rstrip('\n'))
+        stopwords.close()
+        for i in range(12353, 12436):  # 平仮名一文字
+            ngwords.append(chr(i))
+        for i in range(12449, 12533):  # 片仮名一文字
+            ngwords.append(chr(i))
+        for i in range(97, 97+26):  # アルファベット小文字
+            ngwords.append(chr(i))
+        for i in range(65, 65+26):  # アルファベット大文字
+            ngwords.append(chr(i))
+        ngwords_origin = copy.deepcopy(ngwords)
+        pass
 
-  def num_zen_to_han(self, opinions):
-    for i in range(len(opinions)):
-      opinions[i] = mj.zen_to_han(opinions[i], kana=False, ascii=False)
-    return opinions
+    def text_splitter(self, opinions):
+        splitted_opinions = []
+        split_list = ['。・', '？・']
+        split_list2 = ['②', '③', '④', '⑤', '⑥', '⑦', '⑧', '⑨']
+        for opinion in opinions:
+            flag = False
+            if '①' in opinion:
+                for spliter in split_list2:
+                    if spliter in opinion:
+                        opinion_buf = opinion.split(spliter)
+                        splitted_opinions.append(opinion_buf[0][1:])
+                        opinion = opinion_buf[1]
+                        flag = True
+                splitted_opinions.append(opinion)
+            if not flag:
+                for spliter in split_list:
+                    if spliter in opinion:
+                        splitted_opinions.extend(opinion.split(spliter))
+                        flag = True
+            if not flag:
+                splitted_opinions.append(opinion)
+        return splitted_opinions
+
+    def num_zen_to_han(self, opinions):
+        for i in range(len(opinions)):
+            opinions[i] = mj.zen_to_han(opinions[i], kana=False, ascii=False)
+        return opinions
--- test.py
+++ test.py
@@ -6,16 +6,16 @@ c = ClassificateOpinions(['こんにちは。・おはよう', 'こんばんは'
 out = c.text_splitter(['こんにちは。・おはよう', '①こんばんは②そうなのですか'])
 ans = ['こんにちは', 'おはよう', 'こんばんは', 'そうなのですか']
 if out == ans:
-  print('ok1')
+    print('ok1')
 else:
-  print('error1')
-  print('expected is ',ans, ', your out is', out)
+    print('error1')
+    print('expected is ', ans, ', your out is', out)
 
 # def num_zen_to_han
 out = c.num_zen_to_han(['12おはよう', '３４です', 'おは５6'])
 ans = ['12おはよう', '34です', 'おは56']
 if out == ans:
-  print('ok2')
+    print('ok2')
 else:
-  print('error2')
-  print('expected is ',ans, ', your out is', out)
+    print('error2')
+    print('expected is ', ans, ', your out is', out)
