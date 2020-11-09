cedfa8d1bc44b9098cec73d27c9c553b47eab1e7
Takumi Sato <takutaku9090@gmail.com>
2020-11-09 11:37:49 +0900

    add テストの追加

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 from classificate_opinions import ClassificateOpinions
 
 c = ClassificateOpinions(['こんにちは。・おはよう', 'こんばんは'])
-out = c.text_spliter(['こんにちは。・おはよう', 'こんばんは'])
-if out == ['こんにちは', 'おはよう', 'こんばんは']:
+out = c.text_spliter(['こんにちは。・おはよう', '①こんばんは②そうなのですか'])
+if out == ['こんにちは', 'おはよう', 'こんばんは', 'そうなのですか']:
   print('ok')
 else:
   print('error1')
+  print('out is', out)
