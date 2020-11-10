5ea775d3a8361e669f3e90003c2a890bacabce28
Takumi Sato <takutaku9090@gmail.com>
2020-11-10 13:48:13 +0900

    add テストコードを追記

--- test.py
+++ test.py
@@ -1,21 +1,32 @@
 from classificate_opinions import ClassificateOpinions
 
-c = ClassificateOpinions(['こんにちは。・おはよう', 'こんばんは'])
+def res(num, out, ans):
+    if out == ans:
+        print(num, 'ok')
+    else:
+        print(num, 'error')
+        print('expected is ', ans, ', your out is', out)
+    
 
-# def text_spiliter
-out = c.text_splitter(['こんにちは。・おはよう', '①こんばんは②そうなのですか'])
-ans = ['こんにちは', 'おはよう', 'こんばんは', 'そうなのですか']
-if out == ans:
-    print('ok1')
-else:
-    print('error1')
-    print('expected is ', ans, ', your out is', out)
+if __name__=="__main__":
+    c = ClassificateOpinions(['こんにちは。・おはよう', 'こんばんは'])
 
-# def num_zen_to_han
-out = c.num_zen_to_han(['12おはよう', '３４です', 'おは５6'])
-ans = ['12おはよう', '34です', 'おは56']
-if out == ans:
-    print('ok2')
-else:
-    print('error2')
-    print('expected is ', ans, ', your out is', out)
+    # def text_spiliter
+    out = c.text_splitter(['こんにちは。・おはよう', '①こんばんは②そうなのですか'])
+    ans = ['こんにちは', 'おはよう', 'こんばんは', 'そうなのですか']
+    res(1, out, ans)
+
+    # def num_zen_to_han
+    out = c.num_zen_to_han(['12おはよう', '３４です', 'おは５6'])
+    ans = ['12おはよう', '34です', 'おは56']
+    res(2, out, ans)
+
+    # def create_stopwords_list
+    out = len(c.ngwords)
+    ans = 560
+    res(3, out, ans)
+
+    # def tokenize 
+    out = c.tokenize(['今日で子供の家を退園します', '児童クラブを増やして欲しい'])
+    ans = [['今日', '退園', '子供の家'], ['児童クラブ', '増やす']]
+    res(4, out, ans)
