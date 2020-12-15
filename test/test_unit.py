557187aefed0807631f03fe8591f237a6c3b3750
Takumi Sato <takutaku9090@gmail.com>
2020-12-15 17:39:48 +0900

    clean
    run autopep8

--- test/test_unit.py
+++ test/test_unit.py
@@ -1,10 +1,11 @@
+from classificate_opinions import ClassificateOpinions
 import unittest
 import sys
 sys.path.append('./')
-from classificate_opinions import ClassificateOpinions
+
 
 class TestClassificateOpinions(unittest.TestCase):
-    
+
     def setUp(self):
         opinions = ['保育園の数が少なく入れるか不安', '保育園の数を増やしてください！', '学童を増やして欲しいです', 'あ']
         self.c = ClassificateOpinions(opinions)
@@ -14,7 +15,7 @@ class TestClassificateOpinions(unittest.TestCase):
         out = self.c.text_splitter(['こんにちは。・おはよう', '①こんばんは②そうなのですか'])
         ans = ['こんにちは', 'おはよう', 'こんばんは', 'そうなのですか']
         self.assertEqual(out, ans)
-    
+
     def test_num_zen_to_jan(self):
         out = self.c.num_zen_to_han(['12おはよう', '３４です', 'おは５6'])
         ans = ['12おはよう', '34です', 'おは56']
@@ -31,7 +32,8 @@ class TestClassificateOpinions(unittest.TestCase):
         self.assertEqual(out, ans)
 
     def test_tokenize(self):
-        out = self.c.tokenize(['保育園の数が少なく入れるか不安', '保育園の数を増やして！', '学童を増やして欲しいです'])
+        out = self.c.tokenize(
+            ['保育園の数が少なく入れるか不安', '保育園の数を増やして！', '学童を増やして欲しいです'])
         ans = [['保育園', '入れる', '不安'], ['保育園', '増やす'], ['学童', '増やす']]
         self.assertEqual(out, ans)
 
@@ -44,9 +46,10 @@ class TestClassificateOpinions(unittest.TestCase):
         out = self.c.extract_large_cliques([
             ['a', 'b'], ['a', 'c', 'b'], ['c'],
             ['a'], ['a', 'b'], ['a', 'c', 'b', 'd'],
-            ])
-        ans = [['a', 'c', 'b', 'd']] # 最も大きいリストが帰ってくる
+        ])
+        ans = [['a', 'c', 'b', 'd']]  # 最も大きいリストが帰ってくる
         self.assertEqual(out, ans)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     unittest.main()
